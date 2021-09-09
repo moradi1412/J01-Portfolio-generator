f1196de0cbc4ad4094e1cfc02cc605b00885eba5
Moradi1412 <moradi.javid@gmail.com>
2021-09-09 16:47:11 -0700

    first use of inquirer

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-const fs = require('fs');
-const generatePage = require("./src/page-template.js");
+const inquirer = require('inquirer'); 
 
-//capture the command line arguements 
-const profileDataArgs = process.argv.slice(2);
+inquirer.prompt([
+  {type: "input",
+   name:'name', 
+  message: 'what is your name'
+}
+]).then(answer => console.log(answer));
+// console.log (inquirer); 
 
-const [name, github] = profileDataArgs;
+
+// const fs = require('fs');
+// const generatePage = require("./src/page-template.js");
+
+// //capture the command line arguements 
+// const profileDataArgs = process.argv.slice(2);
+
+// const [name, github] = profileDataArgs;
 
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 // const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 
